c83db8681f505ab561d18dfcf449ecc68b2f236e
Jennifer Gifford <jgifford82@gmail.com>
2022-10-11 18:19:48 -0600

    commit tree a55bb2e6facc8b0f59c2d0286ad0129241f9eb9c

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
+require 'pry'
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    def initialize(brand)
+        @brand = brand
+    end
+# binding.pry
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+# learn spec/02_shoe_spec.rb
